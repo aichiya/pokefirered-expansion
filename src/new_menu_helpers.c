846612050bd07248dfedda22b3165a53130eebee
Robin Xyifer <xyifer@gmail.com>
2024-04-04 23:15:43 -0500

    Resized pause menu in order to fit wider font.

--- src/new_menu_helpers.c
+++ src/new_menu_helpers.c
@@ -682,7 +682,7 @@ u8 CreateStartMenuWindow(u8 height)
 {
     if (sStartMenuWindowId == 0xFF)
     {
-        struct WindowTemplate template = SetWindowTemplateFields(0, 0x16, 1, 7, height * 2 - 1, DLG_WINDOW_PALETTE_NUM, 0x13D);
+        struct WindowTemplate template = SetWindowTemplateFields(0, 0x15, 1, 8, height * 2 - 1, DLG_WINDOW_PALETTE_NUM, 0x13D);
         sStartMenuWindowId = AddWindow(&template);
         PutWindowTilemap(sStartMenuWindowId);
     }
