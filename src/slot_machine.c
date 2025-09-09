74016733aa7880964c350d9694974eca92f932dc
Robin Xyifer <xyifer@gmail.com>
2025-09-08 21:47:55 -0500

    WIP partial overhaul of slot machines part 3, both versions playable and 99% done.

--- src/slot_machine.c
+++ src/slot_machine.c
@@ -14,7 +14,7 @@
 #include "constants/songs.h"
 
 #define NUM_REELS 3
-#define REEL_LENGTH 21 // Total number of icons per reel
+#define REEL_LENGTH 15 // Total number of icons per reel
 
 // Only 4 icons are actually visible per reel at a single
 // time, with 1 on deck. Only 3 visible when not spinning.
@@ -50,18 +50,17 @@ enum {
     ICON_7,
     ICON_BAR,
     ICON_CHERRIES,
-    ICON_MAGIKARP,  // Magikarp in FR, Staryu in LG
-    ICON_BIRD,
-    ICON_MOUSE, // Mouse in FR, Slowpoke in LG
+    ICON_MAGIKARP, // Magikarp in Red, Jigglypuff in Blue
+    ICON_BIRD,     // Bird in Red, Diglett in Blue
+    ICON_MOUSE,    // Mouse in Red, Poliwag in Blue
     
 };
 
 enum {
     PAYOUT_NONE,
-    PAYOUT_CHERRIES2,
-    PAYOUT_CHERRIES3,
-    PAYOUT_PIKAPSY,
-    PAYOUT_ROCKET,
+    PAYOUT_CHERRIES,
+    PAYOUT_POKE,
+    PAYOUT_BAR,
     PAYOUT_7,
     NUM_PAYOUT_TYPES
 };
@@ -189,7 +188,9 @@ static bool32 IsSlotMachineSetupTaskActive(u8 taskId);
 static bool8 SlotsTask_GraphicsInit(u8 *state, struct SlotMachineSetupTaskData * ptr);
 static bool8 SlotsTask_FadeOut(u8 *state, struct SlotMachineSetupTaskData * ptr);
 static bool8 SlotsTask_UpdateLineStates(u8 *state, struct SlotMachineSetupTaskData * ptr);
+static bool8 SlotsTask_StartWinningLineFlash(u8 *state, struct SlotMachineSetupTaskData * ptr);
 static bool8 SlotsTask_StopWinningLineFlashTask(u8 *state, struct SlotMachineSetupTaskData * ptr);
+static bool8 SlotsTask_DummySkip(u8 *state, struct SlotMachineSetupTaskData * ptr);
 static bool8 SlotsTask_UpdateCoinsDisplay(u8 *state, struct SlotMachineSetupTaskData * ptr);
 static bool8 SlotsTask_MessageOutOfCoins(u8 *state, struct SlotMachineSetupTaskData * ptr);
 static bool8 SlotsTask_AskQuitPlaying(u8 *state, struct SlotMachineSetupTaskData * ptr);
@@ -251,52 +252,46 @@ static const u8 sRowAttributes[NUM_MATCH_LINES][4] = {
 
 static const u16 sReelBiasChances[][NUM_PAYOUT_TYPES] = {
     {
-        [PAYOUT_NONE]      = 0x1fa1,
-        [PAYOUT_CHERRIES2] = 0x2eab,
-        [PAYOUT_CHERRIES3] = 0x3630,
-        [PAYOUT_PIKAPSY]   = 0x3bd4,
-        [PAYOUT_ROCKET]    = 0x3bfc,
-        [PAYOUT_7]         = 0x0049,
+        [PAYOUT_NONE]     = 0x1fa1,
+        [PAYOUT_CHERRIES] = 0x3630,
+        [PAYOUT_POKE]     = 0x3bd4,
+        [PAYOUT_BAR]      = 0x3bfc,
+        [PAYOUT_7]        = 0x0049,
     },
     {
-        [PAYOUT_NONE]      = 0x1f97,
-        [PAYOUT_CHERRIES2] = 0x2ea2,
-        [PAYOUT_CHERRIES3] = 0x3627,
-        [PAYOUT_PIKAPSY]   = 0x3bca,
-        [PAYOUT_ROCKET]    = 0x3bf8,
-        [PAYOUT_7]         = 0x0049,
+        [PAYOUT_NONE]     = 0x1f97,
+        [PAYOUT_CHERRIES] = 0x3627,
+        [PAYOUT_POKE]     = 0x3bca,
+        [PAYOUT_BAR]      = 0x3bf8,
+        [PAYOUT_7]        = 0x0049,
     },
     {
-        [PAYOUT_NONE]      = 0x1f91,
-        [PAYOUT_CHERRIES2] = 0x2e9b,
-        [PAYOUT_CHERRIES3] = 0x3620,
-        [PAYOUT_PIKAPSY]   = 0x3bc4,
-        [PAYOUT_ROCKET]    = 0x3bf4,
-        [PAYOUT_7]         = 0x0049,
+        [PAYOUT_NONE]     = 0x1f91,
+        [PAYOUT_CHERRIES] = 0x3620,
+        [PAYOUT_POKE]     = 0x3bc4,
+        [PAYOUT_BAR]      = 0x3bf4,
+        [PAYOUT_7]        = 0x0049,
     },
     {
-        [PAYOUT_NONE]      = 0x1f87,
-        [PAYOUT_CHERRIES2] = 0x2e92,
-        [PAYOUT_CHERRIES3] = 0x3617,
-        [PAYOUT_PIKAPSY]   = 0x3bba,
-        [PAYOUT_ROCKET]    = 0x3bef,
-        [PAYOUT_7]         = 0x0050,
+        [PAYOUT_NONE]     = 0x1f87,
+        [PAYOUT_CHERRIES] = 0x3617,
+        [PAYOUT_POKE]     = 0x3bba,
+        [PAYOUT_BAR]      = 0x3bef,
+        [PAYOUT_7]        = 0x0050,
     },
     {
-        [PAYOUT_NONE]      = 0x1f7f,
-        [PAYOUT_CHERRIES2] = 0x2e89,
-        [PAYOUT_CHERRIES3] = 0x360e,
-        [PAYOUT_PIKAPSY]   = 0x3bb2,
-        [PAYOUT_ROCKET]    = 0x3bea,
-        [PAYOUT_7]         = 0x0050,
+        [PAYOUT_NONE]     = 0x1f7f,
+        [PAYOUT_CHERRIES] = 0x360e,
+        [PAYOUT_POKE]     = 0x3bb2,
+        [PAYOUT_BAR]      = 0x3bea,
+        [PAYOUT_7]        = 0x0050,
     },
     {
-        [PAYOUT_NONE]      = 0x1fc9,
-        [PAYOUT_CHERRIES2] = 0x2efc,
-        [PAYOUT_CHERRIES3] = 0x3696,
-        [PAYOUT_PIKAPSY]   = 0x3c49,
-        [PAYOUT_ROCKET]    = 0x3c8b,
-        [PAYOUT_7]         = 0x0073,
+        [PAYOUT_NONE]     = 0x1fc9,
+        [PAYOUT_CHERRIES] = 0x3696,
+        [PAYOUT_POKE]     = 0x3c49,
+        [PAYOUT_BAR]      = 0x3c8b,
+        [PAYOUT_7]        = 0x0073,
     },
 };
 
@@ -304,66 +299,60 @@ static const u8 sReelIconAnimByReelAndPos[NUM_REELS][REEL_LENGTH] = {
     {
         ICON_7,
         ICON_MOUSE,
-        ICON_CHERRIES,
-        ICON_BAR,
-        ICON_BIRD,
         ICON_MAGIKARP,
-        ICON_BIRD,
+        ICON_BAR,
+        ICON_CHERRIES,
         ICON_7,
         ICON_MAGIKARP,
-        ICON_MOUSE,
+        ICON_BIRD,
         ICON_BAR,
         ICON_CHERRIES,
-        ICON_BIRD,
-        ICON_MAGIKARP,
         ICON_7,
-        ICON_BIRD,
-        ICON_BAR,
-        ICON_MAGIKARP,
-        ICON_BIRD
-    }, {
-        ICON_CHERRIES,
-        ICON_MOUSE,
-        ICON_BAR,
-        ICON_CHERRIES,
         ICON_MOUSE,
         ICON_BIRD,
-        ICON_CHERRIES,
-        ICON_MOUSE,
-        ICON_7,
-        ICON_CHERRIES,
         ICON_BAR,
-        ICON_MOUSE,
-        ICON_MAGIKARP,
-        ICON_MOUSE,
         ICON_CHERRIES
     }, {
         ICON_7,
-        ICON_MOUSE,
         ICON_MAGIKARP,
+        ICON_CHERRIES,
         ICON_BIRD,
         ICON_MOUSE,
+        ICON_BAR,
+        ICON_CHERRIES,
         ICON_MAGIKARP,
         ICON_BIRD,
-        ICON_MOUSE,
+        ICON_CHERRIES,
+        ICON_BAR,
         ICON_MAGIKARP,
         ICON_BIRD,
-        ICON_MOUSE,
-        ICON_MAGIKARP,
+        ICON_CHERRIES,
+        ICON_MOUSE
+    }, {
+        ICON_7,
         ICON_BIRD,
-        ICON_MOUSE,
         ICON_MAGIKARP,
+        ICON_CHERRIES,
+        ICON_MOUSE,
+        ICON_BIRD,
+        ICON_MAGIKARP,
+        ICON_CHERRIES,
+        ICON_MOUSE,
+        ICON_BIRD,
+        ICON_MAGIKARP,
+        ICON_CHERRIES,
+        ICON_MOUSE,
+        ICON_BIRD,
         ICON_BAR
     },
 };
 
 static const u16 sPayoutTable[] = {
-    [PAYOUT_NONE]      =   0,
-    [PAYOUT_CHERRIES2] =   2,
-    [PAYOUT_CHERRIES3] =   6,
-    [PAYOUT_PIKAPSY]   =  15,
-    [PAYOUT_ROCKET]    = 100,
-    [PAYOUT_7]         = 300
+    [PAYOUT_NONE]     =   0,
+    [PAYOUT_CHERRIES] =   8,
+    [PAYOUT_POKE]     =  15,
+    [PAYOUT_BAR]      = 100,
+    [PAYOUT_7]        = 300
 };
 
 #if defined(FIRERED)
@@ -611,7 +600,10 @@ bool8 (*const sSlotMachineSetupTasks[])(u8 *, struct SlotMachineSetupTaskData *)
     [SLOTTASK_GFX_INIT] = SlotsTask_GraphicsInit,
     [SLOTTASK_FADEOUT_EXIT] = SlotsTask_FadeOut,
     [SLOTTASK_UPDATE_LINE_LIGHTS] = SlotsTask_UpdateLineStates,
+    [SLOTTASK_ANIM_WIN] = SlotsTask_StartWinningLineFlash,
     [SLOTTASK_END_ANIM_WIN] = SlotsTask_StopWinningLineFlashTask,
+    [SLOTTASK_ANIM_LOSE] = SlotsTask_DummySkip,
+    [SLOTTASK_ANIM_BETTING] = SlotsTask_DummySkip,
     [SLOTTASK_SHOW_AMOUNTS] = SlotsTask_UpdateCoinsDisplay,
     [SLOTTASK_MSG_NO_COINS] = SlotsTask_MessageOutOfCoins,
     [SLOTTASK_ASK_QUIT] = SlotsTask_AskQuitPlaying,
@@ -1066,7 +1058,7 @@ static void MainTask_WinHandlePayout(u8 taskId)
     switch (data[0])
     {
     case 0:
-        if (sSlotMachineState->slotRewardClass == PAYOUT_ROCKET || sSlotMachineState->slotRewardClass == PAYOUT_7)
+        if (sSlotMachineState->slotRewardClass == PAYOUT_BAR || sSlotMachineState->slotRewardClass == PAYOUT_7)
             PlayFanfare(MUS_SLOTS_JACKPOT);
         else
             PlayFanfare(MUS_SLOTS_WIN);
@@ -1329,7 +1321,7 @@ static void StopReel2(u16 whichReel)
     if (numPossiblePositions == 0)
     {
         sSlotMachineState->reel2BiasInPlay = 0;
-        if (sSlotMachineState->machineBias == PAYOUT_ROCKET || sSlotMachineState->machineBias == PAYOUT_7)
+        if (sSlotMachineState->machineBias == PAYOUT_BAR || sSlotMachineState->machineBias == PAYOUT_7)
             pos = 4;
         else
             pos = 0;
@@ -1371,7 +1363,7 @@ static void StopReel3(u16 whichReel)
     }
     if (numPossiblePositions == 0)
     {
-        if (sSlotMachineState->machineBias == PAYOUT_ROCKET || sSlotMachineState->machineBias == PAYOUT_7)
+        if (sSlotMachineState->machineBias == PAYOUT_BAR || sSlotMachineState->machineBias == PAYOUT_7)
             pos = 4;
         else
             pos = 0;
@@ -1499,19 +1491,7 @@ static bool32 OneReelBiasCheck(s32 reelId, s32 reelPos, s32 biasIcon)
                 return FALSE;
         }
         return TRUE;
-    case PAYOUT_CHERRIES2:
-        for (i = 0; i < NUM_MATCH_LINES; i++)
-        {
-            if (icons[sThirdReelBiasCheckIndices[i][0]] == icons[sThirdReelBiasCheckIndices[i][1]] && TestReelIconAttribute(biasIcon, icons[sThirdReelBiasCheckIndices[i][0]]))
-                return FALSE;
-        }
-        for (i = 0; i < 3; i++)
-        {
-            if (TestReelIconAttribute(biasIcon, icons[i]))
-                return TRUE;
-        }
-        return FALSE;
-    case PAYOUT_CHERRIES3:
+    case PAYOUT_CHERRIES:
         for (i = 0; i < NUM_MATCH_LINES; i++)
         {
             if (icons[sThirdReelBiasCheckIndices[i][0]] == icons[sThirdReelBiasCheckIndices[i][1]] && TestReelIconAttribute(biasIcon, icons[sThirdReelBiasCheckIndices[i][0]]))
@@ -1533,12 +1513,11 @@ static bool32 TestReelIconAttribute(s32 attr, s32 icon)
     {
     case PAYOUT_NONE:
         return icon ^ 4 ? TRUE : FALSE;
-    case PAYOUT_CHERRIES2:
-    case PAYOUT_CHERRIES3:
+    case PAYOUT_CHERRIES:
         return icon == ICON_CHERRIES ? TRUE : FALSE;
-    case PAYOUT_PIKAPSY:
-        return icon == ICON_BIRD || icon == ICON_MOUSE ? TRUE : FALSE;
-    case PAYOUT_ROCKET:
+    case PAYOUT_POKE:
+        return icon == ICON_BIRD || icon == ICON_MAGIKARP || icon == ICON_MOUSE ? TRUE : FALSE;
+    case PAYOUT_BAR:
         return icon == ICON_BAR ? TRUE : FALSE;
     case PAYOUT_7:
         return icon == ICON_7 ? TRUE : FALSE;
@@ -1553,12 +1532,13 @@ static u8 ReelIconToPayoutRank(s32 iconId)
     {
     default:
     case ICON_CHERRIES:
-        return PAYOUT_CHERRIES2;
+        return PAYOUT_CHERRIES;
     case ICON_BIRD:
+    case ICON_MAGIKARP:
     case ICON_MOUSE:
-        return PAYOUT_PIKAPSY;
+        return PAYOUT_POKE;
     case ICON_BAR:
-        return PAYOUT_ROCKET;
+        return PAYOUT_BAR;
     case ICON_7:
         return PAYOUT_7;
     }
@@ -1574,7 +1554,7 @@ static void CalcSlotBias(void)
         if (rval < biasChances[i])
             break;
     }
-    if (sSlotMachineState->machineBias < PAYOUT_ROCKET)
+    if (sSlotMachineState->machineBias < PAYOUT_BAR)
     {
         if (sSlotMachineState->biasCooldown == 0)
         {
@@ -2019,6 +1999,11 @@ static bool8 SlotsTask_StopWinningLineFlashTask(u8 * state, struct SlotMachineSe
     return TRUE;
 }
 
+static bool8 SlotsTask_DummySkip(u8 * state, struct SlotMachineSetupTaskData * ptr)
+{
+    return FALSE;
+}
+
 static bool8 SlotsTask_UpdateCoinsDisplay(u8 * state, struct SlotMachineSetupTaskData * ptr)
 {
     UpdateCoinsDisplay();
@@ -2256,7 +2241,7 @@ static void Task_FlashWinningLine(u8 taskId)
             data[4] += 8;
             data[4] &= 0x7F;
             data[5] = gSineTable[data[4]] >> 5;
-            BlendPalettes(1 << PALSLOT_LINE_MATCH, data[5], RGB_BLACK);
+            //BlendPalettes(1 << PALSLOT_LINE_MATCH, data[5], RGB_BLACK);
         }
         else
         {
@@ -2266,7 +2251,7 @@ static void Task_FlashWinningLine(u8 taskId)
                 data[4] = 0;
                 data[5]++;
                 data[5] &= 1;
-                BlendPalettes(1 << PALSLOT_LINE_MATCH, data[5] * 8, RGB_BLACK);
+                //BlendPalettes(1 << PALSLOT_LINE_MATCH, data[5] * 8, RGB_BLACK);
             }
         }
 
@@ -2277,14 +2262,14 @@ static void Task_FlashWinningLine(u8 taskId)
         // Restore match lines to normal color 
         for (i = 0; i < NUM_MATCH_LINES; i++)
         {
-            if (GetWinFlagByLine(i))
-                SetLineState(GetBgTilemapBuffer(2), i, PALSLOT_LINE_NORMAL);
+            //if (GetWinFlagByLine(i))
+            //    SetLineState(GetBgTilemapBuffer(2), i, PALSLOT_LINE_NORMAL);
         }
         
         // Restore payout lights to normal color
-        LoadPalette(&sBg_Pal[1], BG_PLTT_ID(1), sizeof(sBg_Pal[1]));
+        //LoadPalette(&sBg_Pal[1], BG_PLTT_ID(1), sizeof(sBg_Pal[1]));
 
-        CopyBgTilemapBufferToVram(2);
+        //CopyBgTilemapBufferToVram(2);
         data[0]++;
         break;
     case 3:
@@ -2292,6 +2277,12 @@ static void Task_FlashWinningLine(u8 taskId)
             DestroyTask(taskId);
         break;
     }
+}
+
+static bool8 SlotsTask_StartWinningLineFlash(u8 * state, struct SlotMachineSetupTaskData * ptr)
+{
+    CreateTask(Task_FlashWinningLine, 3);
+    return FALSE;
 }
 
 static void SignalStopWinningLineFlashTask(void)
